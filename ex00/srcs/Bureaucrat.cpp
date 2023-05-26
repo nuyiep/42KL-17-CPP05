03b024e9015bdd0a5fdba9d2607cda53a4a94ea1
Pei Yun Lau <plau@u91z08s01.42kl.edu.my>
2023-05-26 13:32:04 +0800

    ex00 update

--- ex00/srcs/Bureaucrat.cpp
+++ ex00/srcs/Bureaucrat.cpp
@@ -6,10 +6,72 @@
 /*   By: plau <plau@student.42.kl>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 18:10:01 by plau              #+#    #+#             */
-/*   Updated: 2023/05/16 18:18:48 by plau             ###   ########.fr       */
+/*   Updated: 2023/05/16 20:27:05 by plau             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+Bureaucrat::Bureaucrat()
+{
+	this->_grade = 0;
+}
 
+Bureaucrat::~Bureaucrat()
+{
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &src)
+{
+	(*this) = src;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat &src)
+{
+	this->_grade = src._grade;
+	return (*this);
+}
+
+std::string Bureaucrat::getName() const
+{
+	return (this->_name);
+}
+
+int	Bureaucrat::getGrade() const
+{
+	return (this->_grade);
+}
+
+/* Since grade 1 is the highest and 150 is the lowest */
+/* incrementing a grade 3 will give a grade 2 */
+void Bureaucrat::incrementGrade(int num)
+{
+	int	temp = this->_grade;
+	temp = temp - num;
+	if (errorCheckGrade(temp) == 1)
+		this->_grade = this->_grade - num;
+}
+
+/* Decrementing a grade 3 will give a grade 4 */
+void Bureaucrat::decrementGrade(int num)
+{
+	int	temp = this->_grade;
+	temp = temp + num;
+	if (errorCheckGrade(temp) == 1)
+		this->_grade = this->_grade + num;
+}
+
+int Bureaucrat::errorCheckGrade(int temp)
+{
+	if (temp < 1)
+	{
+		std::cout << "Bureaucrat::GradeTooLowException" << std::endl;
+		return (0);
+	}
+	else if (temp > 150)
+	{
+		std::cout << "Bureaucrat::GradeTooHighException" << std::endl;
+		return (0);
+	}
+	return (1);
+}
--- ex00/srcs/Bureaucrat.hpp
+++ ex00/srcs/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: plau <plau@student.42.kl>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 18:09:40 by plau              #+#    #+#             */
-/*   Updated: 2023/05/16 18:16:16 by plau             ###   ########.fr       */
+/*   Updated: 2023/05/16 20:24:22 by plau             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,29 @@
 # define BUREAUCRAT_HPP
 
 #include <iostream>
+#include <string.h>
 
+class Bureaucrat
+{
+	public:
+		/* Orthodox canonical form */
+		Bureaucrat();
+		~Bureaucrat();
+		Bureaucrat(const Bureaucrat &src);
+		Bureaucrat& operator=(const Bureaucrat &src);
+
+		/* Required by PDF */
+		std::string	getName() const;
+		int			getGrade() const;
+		void		incrementGrade(int num);
+		void		decrementGrade(int num);
+
+		/* Member functions */
+		int			errorCheckGrade(int temp);
+		
+	private:
+		const	std::string	_name;
+		int					_grade;
+};
 
 #endif
