424df381bc70c9a014d3084f0f10733dd8504dbc
Pei Yun Lau <plau@u91z08s01.42kl.edu.my>
2023-07-06 15:03:27 +0800

    Added exception classes and getters functions

--- ex01/srcs/Form.cpp
+++ ex01/srcs/Form.cpp
@@ -6,13 +6,13 @@
 /*   By: plau <plau@student.42.kl>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 18:16:48 by plau              #+#    #+#             */
-/*   Updated: 2023/07/06 12:37:51 by plau             ###   ########.fr       */
+/*   Updated: 2023/07/06 14:52:54 by plau             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
 
-Form::Form() :_name("John Doe"), _signGrade(75), _executeGrade(130)
+Form::Form() :_name("John Doe"), _signGrade(LOWEST_GRADE), _executeGrade(LOWEST_GRADE)
 {
 	this->_signed = false;
 }
@@ -21,18 +21,57 @@ Form::~Form()
 {
 }
 
-Form::Form(const Form &src)
+Form::Form(const Form &src) :_name("PY"), _signGrade(70), _executeGrade(130)
 {
 	(*this) = src;
 }
 
 Form &Form::operator=(const Form &src)
 {
-	(void)src;
+	this->_signed = src._signed;
 	return (*this);
 }
 
-std::ostream &operator<<(std::ostream *os, Form const &obj)
+std::string Form::getName() const
 {
-	
+	return (this->_name);
 }
+
+bool Form::getSigned() const
+{
+	return (this->_signed);
+}
+
+int Form::getSignGrade() const
+{
+	return (this->_signGrade);
+}
+
+int Form::getExecuteGrade() const
+{
+	return (this->_executeGrade);
+}
+
+/* Provides the definition of what() */
+std::ostream &operator<<(std::ostream &os, Form const &obj)
+{
+	os << BOLD_CYAN "Name: \t\t\t\t" << obj.getName() << std::endl
+		<< BOLD_GREEN "Has/has not signed: \t\t" << obj.getSigned() << std::endl
+		<< "Grade required to sign: \t" << obj.getSignGrade() << std::endl
+		<< "Grade required to execute: \t" << obj.getExecuteGrade() << std::endl
+		<< std::endl;
+		RESET;
+	return (os);
+}
+
+const char* Form::GradeTooHighException::what() const throw()
+{
+	return (BOLD_MAGENTA "[Form] Grade too high." RESET);
+}
+
+const char *Form::GradeTooLowException::what() const throw()
+{
+	return (BOLD_RED "[Form] Grade too low." RESET);
+}
+
+
--- ex01/srcs/Form.hpp
+++ ex01/srcs/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: plau <plau@student.42.kl>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 18:16:51 by plau              #+#    #+#             */
-/*   Updated: 2023/07/06 13:20:12 by plau             ###   ########.fr       */
+/*   Updated: 2023/07/06 14:50:39 by plau             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,29 @@ class Form
 		/* Getters - adding const behind- indicates that it */
 		/* will not modify the state of the object  */
 		/* but it allows the modification of the returned value */
-		std::string		   &getName() const;
+		std::string		    getName() const;
 		bool			   	getSigned() const;
 		int				    getSignGrade() const;
-		const int					getExecuteGrade();
+		int					getExecuteGrade() const;
 		
-
+		/* Member functions */
+		// Form::Form(std::string name, int grade)
+		// {
+			
+		// }
+		
+		/* Exception classes */
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+		
 	private:
 		const std::string 	_name;
 		bool 				_signed;
